b68103211db5e421ff62b31af889eb6cb933db4f
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-11-10 15:43:57 +0530

    [befourProject] - challenge done!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+let userName = "Arjun ";
+let userDetails = prompt("What your name ?");
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Emoji-Pedia</h1>
+      <h2 style={{ fontSize: "2rem", color:"red" }}>Welcome,{userName}</h2>
+      <h3>{"You'r Name is "+ userDetails}</h3>
     </div>
   );
 }
