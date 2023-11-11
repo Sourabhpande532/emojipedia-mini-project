d7fcef2b5229201a067c81bc3b5ccdfa70a30521
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-11-11 15:43:37 +0530

    fix - output:showMeaning while taking input

--- src/App.js
+++ src/App.js
@@ -2,26 +2,45 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
-let userCounter = 0;
 let userName = "Arjun ";
 
-let userDetails = prompt("What your name ?");
+// let userDetails = prompt("What your name ?");
+let increment = 0;
+
+let emojiDictionary = {
+  "🫡": "salute",
+  "😍": "On the Moon",
+  "😅": "Rat a smell",
+};
+console.log(emojiDictionary["😍"]);
 
 function App() {
-  const [count,setCount] = useState(0);
-  const getclick = () => {
-    userCounter = userCounter + 1;
-    setCount(userCounter)
+  const [count, setCount] = useState(0);
+  const [search, setSearch] = useState("");
+
+  const getClickedForLiked = () => {
+    increment += 1;
+    setCount(increment);
+  };
+
+  const readUserValue = (event) => {
+   let userInputQuerry = event.target.value;
+   let userInputEmoji = emojiDictionary[userInputQuerry]
+   if(userInputEmoji === undefined){
+    userInputEmoji = "Currently Not Avalable"
+   }
+   setSearch(userInputEmoji)
   };
 
   return (
     <div className='App'>
       <h1>Emoji-Pedia</h1>
       <h2 style={{ fontSize: "2rem", color: "red" }}>Welcome,{userName}</h2>
-      <h3>{"You'r Name is " + userDetails}</h3>
-
-      <button onClick={getclick}>Like</button>
-      <div>{count}</div>
+      <button onClick={getClickedForLiked}>Like</button>
+      <small>{count}</small>
+      <hr></hr>
+      <input type='text' placeholder='type here..' onChange={readUserValue} />
+      <h1>{search}</h1>
     </div>
   );
 }
